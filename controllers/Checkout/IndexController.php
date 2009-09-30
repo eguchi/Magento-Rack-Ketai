69f2264a41e75ba8253ffddcefe815409693670c
hirokazu nishi <rack990@gmail.com>
2009-10-01 00:04:13 +0900

    remove unuse files

--- controllers/Checkout/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Kdl_Ketai_Checkout_IndexController extends Kdl_Ketai_Controller_Common
-{
-    function indexAction()
-    {
-        $this->_redirect('checkout/onepage', array('_secure'=>true));
-    }
-}
