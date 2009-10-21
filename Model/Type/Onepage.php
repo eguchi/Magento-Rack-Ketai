b9a80a1a7f96d99782841e4f27975a3b6ea1adf1
hirokazu nishi <rack990@gmail.com>
2009-10-22 07:36:04 +0900

    imprement save process

--- Model/Type/Onepage.php
+++ Model/Type/Onepage.php
@@ -339,9 +339,9 @@ class Rack_Ketai_Model_Type_Onepage extends Mage_Checkout_Model_Type_Onepage
     protected function validateOrder()
     {
         $helper = Mage::helper('checkout');
-        if ($this->getQuote()->getIsMultiShipping()) {
-            Mage::throwException($helper->__('Invalid checkout type.'));
-        }
+        //if ($this->getQuote()->getIsMultiShipping()) {
+        //    Mage::throwException($helper->__('Invalid checkout type.'));
+        //}
 
         if (!$this->getQuote()->isVirtual()) {
             $address = $this->getQuote()->getShippingAddress();
