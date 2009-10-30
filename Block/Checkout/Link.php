2e081539a6925ce73b4bf1743ccce8371c970183
akihiro eguchi <eguchi.a.jp@gmail.com>
2009-10-30 17:48:36 +0900

    fix checkout link

--- Block/Checkout/Link.php
+++ Block/Checkout/Link.php
@@ -19,4 +19,34 @@ class Rack_Ketai_Block_Checkout_Link extends Mage_Checkout_Block_Onepage_Link
 
         return parent::_toHtml();
     }
+
+    public function addCartLink()
+    {
+        if ($parentBlock = $this->getParentBlock()) {
+            $count = $this->helper('checkout/cart')->getSummaryCount();
+
+            if( $count == 1 ) {
+                $text = $this->__('My Cart (%s item)', $count);
+            } elseif( $count > 0 ) {
+                $text = $this->__('My Cart (%s items)', $count);
+            } else {
+                $text = $this->__('My Cart');
+            }
+
+            $parentBlock->addLink($text, 'ketai/cart', $text, true, array(), 50, null, 'class="top-link-cart"');
+        }
+        return $this;
+    }
+
+    public function addCheckoutLink()
+    {
+        if (!$this->helper('checkout')->canOnepageCheckout()) {
+            return $this;
+        }
+        if ($parentBlock = $this->getParentBlock()) {
+            $text = $this->__('Checkout');
+            $parentBlock->addLink($text, 'ketai/checkout', $text, true, array(), 60, null, 'class="top-link-checkout"');
+        }
+        return $this;
+    }
 }
